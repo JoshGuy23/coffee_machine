0463c7082b7525d7d6aebe510906065330436290
Joshua Hecker <jhecker2001@gmail.com>
2023-10-23 09:20:47 -0500

    Added functionality for "off" command.

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -10,11 +10,20 @@ from reference import resources
 # TODO: 7. Make coffee
 
 
-def command():
+def command(prompt):
     """Executes a user command"""
-    print()
+    if prompt == "off":
+        return -1
 
 
 def coffee_machine():
     """Coffee Machine"""
     prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    status = command(prompt)
+    if status == -1:
+        return 0
+    
+
+running = 1
+while running == 1:
+    running = coffee_machine()
