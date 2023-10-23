cfa049c071724e6f5e853a8449e0f129b5280a49
Joshua Hecker <jhecker2001@gmail.com>
2023-10-23 09:15:31 -0500

    Created command and coffee_machine functions, added user prompt.

--- coffee_machine/main.py
+++ coffee_machine/main.py
@@ -1,2 +1,18 @@
 from reference import MENU
 from reference import resources
+
+# TODO: 1. Prompt user for coffee
+# TODO: 2. Turn off coffee machine from 'off' prompt
+# TODO: 3. Print report
+# TODO: 4. Check that resources are sufficient
+# TODO: 5. Process coins
+# TODO: 6. Check transaction successful
+# TODO: 7. Make coffee
+
+
+def command():
+    print()
+
+
+def coffee_machine():
+    prompt = input("What would you like? (espresso/latte/cappuccino): ").lower()
